3fba690376c2bd0076a235085cb8ace03aa1a1be
Ritta <brittanykelly33@gmail.com>
2022-05-03 13:07:01 -0400

    first push

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    createToDo(e.target["new-task-description"].value);
+    form.reset();
+  })
 });
+
+function createToDo(todo) {
+  const btn = document.createElement('button')
+  document.getElementById("list");
+  list.innerHTML += `<li>${todo} <button>${btn}</button></li>`;
+  document.querySelector('button').textContent = ' x'
+  document.querySelector('button').addEventListener('click', handleDelete);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
+
